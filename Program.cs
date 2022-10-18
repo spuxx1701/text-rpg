08daac53802217176c83403056cc8dc890bdd704
Spuxx <ameisenfutter@outlook.com>
2022-10-18 20:45:25 +0200

    first lesson

--- Program.cs
+++ Program.cs
@@ -2,10 +2,23 @@
 using System;
 using System.Collections.Generic;
 
-namespace text_rpg {
-  class program {
-    static void Main() {
-      Console.WriteLine("Game starting.");
+namespace text_rpg
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Elk Elchson Productions presents: The Awesome Text RPG.");
+      string player = SetupPlayer();
+      Console.WriteLine("Game ends.");
+    }
+
+    static string SetupPlayer()
+    {
+      Console.WriteLine("Welcome! Please name your hero:");
+      string playerName = Console.ReadLine();
+      Console.WriteLine("Hello, " + playerName + "!");
+      return playerName;
     }
   }
 }
